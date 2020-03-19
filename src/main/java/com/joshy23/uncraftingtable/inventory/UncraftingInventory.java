78711adb0986918c2d44f54db2df43f29aee4202
joshy23 <58797123+joshy56@users.noreply.github.com>
2020-03-19 16:49:12 -0300

    inventory work
    the inventory is working in the frontend

--- src/main/java/com/joshy23/uncraftingtable/inventory/UncraftingInventory.java
+++ src/main/java/com/joshy23/uncraftingtable/inventory/UncraftingInventory.java
@@ -13,11 +13,15 @@ public class UncraftingInventory {
     private final int[] grid = {14,15,16,23,24,25,32,33,34};
 
     public UncraftingInventory(Player player){
-        inventory = Bukkit.createInventory(player, 54, ColorUtil.color("&2&m<<&3&lUncraftingTable &2&m>>"));
-        inventory.all(ItemUtil.createItem("stained_glass_pane", 1, 8, ""));
-        for(int i = 0, c = 45; i<=9 && c<=inventory.getSize()-1; i++, c++){
-            inventory.setItem(i, ItemUtil.createItem("barrier", 1));
-            inventory.setItem(c, ItemUtil.createItem("stained_glass_pane", 1, 14, ""));
+        inventory = Bukkit.createInventory(player, 54, ColorUtil.color("&2&m<<&3&lUncraftingTable&2&m>>"));
+        for (int i = 0; i < inventory.getSize()-1; i++) {
+            inventory.setItem(i, ItemUtil.createItem("stained_glass_pane", 1, 7, " "));
+        }
+        for (int i = 45; i < 54; i++) {
+            inventory.setItem(i, ItemUtil.createItem("stained_glass_pane", 1, 14, " "));
+        }
+        for(int i:grid){
+            inventory.setItem(i, new ItemStack(Material.BARRIER, 1));
         }
         inventory.setItem(21, new ItemStack(Material.AIR, 1));
         inventory.setItem(49, ItemUtil.createItem("barrier", 1, 0, "&c&lCERRAR"));
